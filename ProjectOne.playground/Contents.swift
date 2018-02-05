0e583c526770faa8333181aab08a8b2cbe091566
CM Student <cmstudent@UHWO-ACM-C-03.local>
2018-02-05 11:43:48 -1000

    done till part 5

--- ProjectOne.playground/Contents.swift
+++ ProjectOne.playground/Contents.swift
@@ -6,4 +6,15 @@ var str = "Hello, playground"
 
 //Variable Coding Challenge #1
 
+var firstString: String
+firstString = "I like pie"
 
+let secondString = "no way"
+
+var myThirdVariable = firstString + secondString
+print( myThirdVariable
+
+)
+//Variable coding challenge #2
+
+let celsius : Float
